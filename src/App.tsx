266de1de082b21fe459cd2f4b7a14efb0bfd38fe
piyushdps <piyush.aryan@smallcase.com>
2024-08-10 18:54:31 +0530

    chore: update readme and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import { redirect } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import PostList from "./components/PostList";
@@ -8,7 +8,9 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <PostList />,
+      loader: async () => {
+        return redirect("/login");
+      },
     },
     {
       path: "/login",
